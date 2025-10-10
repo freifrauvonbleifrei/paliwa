db13bde1c453c60b42eef2c270d877af1a9dd568
Theresa Pollinger <theresa.pollinger@riken.jp>
2025-10-10 07:32:55 +0200

    src: one hierarchization

--- src/src.cpp
+++ src/src.cpp
@@ -139,6 +139,25 @@ SDDom strided_domain_from_level(std::array<int, dimensionality> const& level, st
     return SDDom(lbound_all, resolution_all, strides_all);
 }
 
+template <typename DDimInWhichItsOdd>
+SDDom odd_strided_domain_from_domain(SDDom const& domain) {
+    ddc::DiscreteVector<DDimInWhichItsOdd> odd_offset(domain.strides().get<DDimInWhichItsOdd>());
+    DVect strides_odd = domain.strides();
+    strides_odd.get<DDimInWhichItsOdd>() *= 2;
+    auto extent_odd = domain.extents();
+    extent_odd.get<DDimInWhichItsOdd>() = (extent_odd.get<DDimInWhichItsOdd>()) / 2;
+    return SDDom(lbound_all + odd_offset, extent_odd, strides_odd);
+}
+
+template <typename DDimInWhichItsEven>
+SDDom even_strided_domain_from_domain(SDDom const& domain) {
+    DVect strides_even = domain.strides();
+    strides_even.get<DDimInWhichItsEven>() *= 2;
+    auto extent_even = domain.extents();
+    extent_even.get<DDimInWhichItsEven>() = (extent_even.get<DDimInWhichItsEven>()) / 2;
+    return SDDom(lbound_all, extent_even, strides_even);
+}
+
 template <typename ChunkType>
 void dump_chunk_span_to_binary_file(ChunkType const span, std::string const& filename){
     std::ofstream file(filename, std::ios::trunc | std::ios::binary);
@@ -167,20 +186,20 @@ int main(int argc, char* argv[])
     auto const x_domain_with_periodic_point = ddc::init_discrete_space<DDimX>(DDimX::init<DDimX>(
             ddc::Coordinate<X>(x_start),
             ddc::Coordinate<X>(x_end),
-            ddc::DiscreteVector<DDimX>(resolution[0] + 1)));
+            ddc::DiscreteVector<DDimX>(resolution[0])));
     ddc::DiscreteDomain<DDimX> const x_domain
             = x_domain_with_periodic_point.remove_last(ddc::DiscreteVector<DDimX>(1));
     auto const y_domain_with_periodic_point = ddc::init_discrete_space<DDimY>(DDimY::init<DDimY>(
             ddc::Coordinate<Y>(y_start),
             ddc::Coordinate<Y>(y_end),
-            ddc::DiscreteVector<DDimY>(resolution[1] + 1)));
+            ddc::DiscreteVector<DDimY>(resolution[1])));
     ddc::DiscreteDomain<DDimY> const y_domain
             = y_domain_with_periodic_point.remove_last(ddc::DiscreteVector<DDimY>(1));
 #if DIMENSIONALITY > 2
     auto const z_domain_with_periodic_point = ddc::init_discrete_space<DDimZ>(DDimZ::init<DDimZ>(
             ddc::Coordinate<Z>(z_start),
             ddc::Coordinate<Z>(z_end),
-            ddc::DiscreteVector<DDimZ>(resolution[2] + 1)));
+            ddc::DiscreteVector<DDimZ>(resolution[2])));
     ddc::DiscreteDomain<DDimZ> const z_domain
             = z_domain_with_periodic_point.remove_last(ddc::DiscreteVector<DDimZ>(1));
 
@@ -232,5 +251,46 @@ int main(int argc, char* argv[])
         dump_chunk_span_to_binary_file(strided_grid, filename);
         // std::cout << strided_grid << std::endl; (-> issue)
         ddc::print_content(std::cout, strided_grid) << std::endl;
+    }
+
+    // hierarchize / wavelet-ify
+    for (int grid_index = 0; grid_index < all_levels.size(); ++ grid_index){
+        auto& level = all_levels[grid_index];
+        auto coefficient = all_combi_coefficients[grid_index];
+        SDDom const& strided_domain = component_grid_domains[grid_index];
+        auto strided_grid = level_data[grid_index].span_view();
+
+        // the finest stride
+        assert((1 << (maximum_level[0] - level[0])) == strided_domain.strides().get<DDimX>());
+
+        std::array<double, 3> const odd_filter = {-0.5, 1.0, -0.5};
+
+        // start with the first dimension and the finest level
+        auto odd_strided_domain = odd_strided_domain_from_domain<DDimX>(strided_domain);
+        for (int current_level = level[0]; current_level > minimum_level[0]; --current_level){
+            int const current_stride = (1 << (maximum_level[0] - current_level));
+            int const next_coarser_stride = current_stride << 1;
+            ddc::parallel_for_each(
+                odd_strided_domain,
+                KOKKOS_LAMBDA(DElem const ixyz) {
+                    // how to access / slice at every other point in x?
+                    // check for out of bounds
+                    if (DElemX(ixyz) + DVectX(current_stride) <= DElemX(odd_strided_domain.back())){ 
+                        strided_grid(ixyz) = 
+                            odd_filter[0] * strided_grid(ixyz - DVectX(current_stride)) +
+                            odd_filter[1] * strided_grid(ixyz) +
+                            odd_filter[2] * strided_grid(ixyz + DVectX(current_stride));
+                    } else {
+                        // on the upper boundary, no +1 available 
+                        // TODO make separate step to avoid branch here
+                        auto wraparound = DElem(DElemX(lbound_all), DElemY(ixyz));
+                        strided_grid(ixyz) = 
+                            odd_filter[0] * strided_grid(ixyz - DVectX(current_stride)) +
+                            odd_filter[1] * strided_grid(ixyz) +
+                            odd_filter[2] * strided_grid(wraparound);
+                    }
+                });
+            odd_strided_domain = odd_strided_domain_from_domain<DDimX>(odd_strided_domain);
+        }
     }
 }
