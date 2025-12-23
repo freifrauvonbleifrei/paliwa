77c81a2b9dbe396e747261560769e1e38ad3a974
Theresa Pollinger <theresa.pollinger@riken.jp>
2025-12-23 15:22:32 +0900

    test domains: test for 2d (unfinished!)

--- src/paliwa_domains.hpp
+++ src/paliwa_domains.hpp
@@ -194,30 +194,35 @@ constexpr auto restrict_strided_with_discrete(
 }
 
 template <typename DDim, typename DomainType>
-constexpr auto restrict_sparse_with_other_domain(
+constexpr ddc::SparseDiscreteDomain<DDim> restrict_sparse_with_other_domain(
     ddc::SparseDiscreteDomain<DDim> const &sparse_domain,
     DomainType const &other_domain) {
   using DElem = ddc::DiscreteElement<DDim>;
   Kokkos::View<DElem *, Kokkos::SharedSpace> elements(
       "restricted_sparse_elements", sparse_domain.size());
+  auto other_domain_projected = ddc::select<DDim>(other_domain);
   size_t insert_index = 0;
-  ddc::host_for_each(sparse_domain,
-                     [&elements, &insert_index, &other_domain](DElem ixyz) {
-                       if (other_domain.contains(ixyz)) {
-                         elements(insert_index++) = ixyz;
-                       }
-                     });
+  ddc::host_for_each(sparse_domain, [&elements, &insert_index,
+                                     &other_domain_projected](DElem ixyz) {
+    if (other_domain_projected.contains(ixyz)) {
+      elements(insert_index++) = DElem(ixyz);
+    }
+  });
   Kokkos::resize(elements, insert_index);
   return ddc::SparseDiscreteDomain<DDim>(elements);
 }
 
-template <typename... DDims, typename DomainType,
-          typename = std::enable_if_t<sizeof...(DDims) >= 2, int>>
-constexpr auto restrict_sparse_with_other_domain(
-    ddc::SparseDiscreteDomain<DDims...> const &sparse_domain,
+template <typename FirstDim, typename... DDims, typename DomainType,
+          typename = std::enable_if_t<sizeof...(DDims) >= 1, int>>
+constexpr ddc::SparseDiscreteDomain<FirstDim, DDims...>
+restrict_sparse_with_other_domain(
+    ddc::SparseDiscreteDomain<FirstDim, DDims...> const &sparse_domain,
     DomainType const &other_domain) {
-  return ddc::SparseDiscreteDomain<DDims...>(restrict_sparse_with_other_domain(
-      ddc::select<DDims>(sparse_domain), other_domain)...);
+  return ddc::SparseDiscreteDomain<FirstDim, DDims...>(
+      restrict_sparse_with_other_domain<FirstDim>(
+          ddc::select<FirstDim>(sparse_domain), other_domain),
+      restrict_sparse_with_other_domain<DDims...>(
+          ddc::select<DDims...>(sparse_domain), other_domain));
 }
 
 template <typename DDim>
--- test/test_paliwa_domains.cpp
+++ test/test_paliwa_domains.cpp
@@ -10,13 +10,14 @@
 #include "../src/paliwa_domains.hpp"
 
 struct X {};
+struct Y {};
 struct DDimX : ddc::UniformPointSampling<X> {};
+struct DDimY : ddc::UniformPointSampling<Y> {};
 
-TEST(domain, sparse_domain_conversion_1d) {
-  using DElem = ddc::DiscreteElement<DDimX>;
-  ddc::DiscreteDomain<DDimX> local_domain =
-      ddc::DiscreteDomain<DDimX>(DElem(1), ddc::DiscreteVector<DDimX>(200));
-
+template <typename DDim>
+ddc::SparseDiscreteDomain<DDim>
+random_sparse_subdomain(ddc::DiscreteDomain<DDim> local_domain) {
+  using DElem = ddc::DiscreteElement<DDim>;
   Kokkos::View<DElem *, Kokkos::SharedSpace> elements("elements",
                                                       local_domain.size());
 
@@ -33,7 +34,16 @@ TEST(domain, sparse_domain_conversion_1d) {
                        }
                      });
   Kokkos::resize(elements, insert_index);
-  ddc::SparseDiscreteDomain<DDimX> sparse_domain(elements);
+  return ddc::SparseDiscreteDomain<DDim>(elements);
+}
+
+TEST(domain, sparse_domain_conversion_1d) {
+  using DElem = ddc::DiscreteElement<DDimX>;
+  ddc::DiscreteDomain<DDimX> local_domain =
+      ddc::DiscreteDomain<DDimX>(DElem(1), ddc::DiscreteVector<DDimX>(200));
+
+  ddc::SparseDiscreteDomain<DDimX> sparse_domain =
+      random_sparse_subdomain(local_domain);
 
   ddc::StridedDiscreteDomain<DDimX> strided_domain(
       DElem(20), ddc::DiscreteVector<DDimX>(20), ddc::DiscreteVector<DDimX>(3));
@@ -62,4 +72,46 @@ TEST(domain, sparse_domain_conversion_1d) {
                          EXPECT_FALSE(restricted_sparse_domain.contains(ixyz));
                        }
                      });
+}
+
+TEST(domain, sparse_domain_conversion_2d) {
+  using DElem = ddc::DiscreteElement<DDimX, DDimY>;
+  ddc::DiscreteDomain<DDimX, DDimY> local_domain(
+      ddc::DiscreteDomain<DDimX>(ddc::DiscreteElement<DDimX>(1),
+                                 ddc::DiscreteVector<DDimX>(20)),
+      ddc::DiscreteDomain<DDimY>(ddc::DiscreteElement<DDimY>(5),
+                                 ddc::DiscreteVector<DDimY>(30)));
+  ddc::SparseDiscreteDomain<DDimX, DDimY> sparse_domain(
+      random_sparse_subdomain(ddc::select<DDimX>(local_domain)),
+      random_sparse_subdomain(ddc::select<DDimY>(local_domain)));
+
+  ddc::StridedDiscreteDomain<DDimX, DDimY> strided_domain(
+      ddc::DiscreteElement<DDimX, DDimY>(std::array<int, 2>({10, 10})),
+      ddc::DiscreteVector<DDimX, DDimY>(std::array<long int, 2>({10, 10})),
+      ddc::DiscreteVector<DDimX, DDimY>(std::array<long int, 2>({2, 3})));
+  // test union
+  ddc::SparseDiscreteDomain<DDimX, DDimY> sparse_and_strided_domain =
+      paliwa::union_of_sparse_domains(
+          paliwa::sparse_from_strided_domain(strided_domain), sparse_domain);
+  ddc::host_for_each(strided_domain, [&sparse_and_strided_domain](DElem ixyz) {
+    EXPECT_TRUE(sparse_and_strided_domain.contains(ixyz));
+  });
+  ddc::host_for_each(sparse_domain, [&sparse_and_strided_domain](DElem ixyz) {
+    EXPECT_TRUE(sparse_and_strided_domain.contains(ixyz));
+  });
+//   auto first_dim_sparse_and_strided = //TODO why does this fail? -> needed to make the rest work
+//       ddc::select<DDimX>(sparse_and_strided_domain);
+  //   // also test intersection
+  //   ddc::SparseDiscreteDomain<DDimX, DDimY> restricted_sparse_domain =
+  //       paliwa::restrict_sparse_with_other_domain(sparse_domain,
+  //       strided_domain);
+  //   ddc::host_for_each(sparse_domain,
+  //                      [&strided_domain, &restricted_sparse_domain](DElem
+  //                      ixyz) {
+  //                        if (strided_domain.contains(ixyz)) {
+  //                          EXPECT_TRUE(restricted_sparse_domain.contains(ixyz));
+  //                        } else {
+  //                          EXPECT_FALSE(restricted_sparse_domain.contains(ixyz));
+  //                        }
+  //                      });
 }
