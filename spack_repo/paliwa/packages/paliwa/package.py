60244df29cd949c6f7cd1def71033f62e0b9228e
Theresa Pollinger <theresa.pollinger@riken.jp>
2025-12-23 16:36:47 +0900

    install: add spack_repo w/ package

---
+++ spack_repo/paliwa/packages/paliwa/package.py
@@ -0,0 +1,36 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack_repo.builtin.build_systems.cmake import CMakePackage
+
+from spack.package import *
+
+class Paliwa(CMakePackage):
+    """paliwa is a library for parallel lifting wavelet transforms."""
+
+    git = "https://github.com/freifrauvonbleifrei/paliwa.git"
+
+    maintainers("freifrauvonbleifrei")
+
+    license("LGPL-3.0-or-later", checked_by="freifrauvonbleifrei")
+
+    version("main", branch="main")
+
+    variant("mpi", default=True, description="Enable MPI support")
+    variant("tests", default=False, description="Build tests")
+
+    depends_on("cxx", type="build")
+    depends_on("cmake@3.25:", type="build")
+    depends_on("ddc~splines~fft")
+    depends_on("googletest", when="+tests", type="test")
+    depends_on("kokkos")
+    depends_on("mpi", when="+mpi")
+
+    def cmake_args(self):
+        args = [
+            self.define("PALIWA_BUILD_EXAMPLES", False),
+            self.define_from_variant("PALIWA_BUILD_TESTS", "tests"),
+            self.define_from_variant("PALIWA_WITH_MPI", "mpi")
+        ]
+        return args
